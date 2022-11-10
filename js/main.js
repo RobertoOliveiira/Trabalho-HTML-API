39387a923635394296082a2c768d8d75f6b4b9d9
Roberto Oliveira <roberto.oliveira1999@hotmail.com>
2022-11-09 21:49:24 -0300

    finalização

--- js/main.js
+++ js/main.js
@@ -1,9 +1,57 @@
 const URL_API = 'https://psychonauts-api.herokuapp.com/api/characters?limit=30';
+let URL_BUSCA = 'https://psychonauts-api.herokuapp.com/api/characters?name=';
 
 $(document).ready(() => {
     getPersonagens();
 })
 
+const buscar = () =>{
+    let nome = $('#pesquisar').val();
+    URL_BUSCA += nome;
+    //console.log(nome);
+    $.ajax({
+        url: URL_BUSCA,
+        dataType: 'json',
+        success: (data) => {
+        //console.log(data);
+            let listPersonagens = document.createElement('div');
+            $(listPersonagens).addClass('galeria-container');
+            $('#personagens').html(listPersonagens);
+
+                let per = document.createElement('a');
+                let li = document.createElement('div');
+                let card = document.createElement('div');
+                let cardHeader = document.createElement('div');
+                let cardBody = document.createElement('div');
+                let dataHtml = document.createElement('img');
+                $(li).addClass('galeria-personagem');
+                $(per).addClass('lista-personagens');
+                $(card).addClass('card');
+                $(cardHeader).addClass('card-header');
+                $(cardBody).addClass('card-body');
+                $(cardHeader).attr('id', `per${0}`);
+                $(cardBody).html(`<h2>${data.name}</h2>`);
+                $(card).append(cardHeader)
+                    .append(cardBody);
+                $(li).append(card);
+                $(dataHtml).addClass('img-top');
+                $(dataHtml).attr('src',data.img);
+                $(per).attr('href', 'character.html');
+                $(per).attr('onClick', "setName('"+data.name+"')")
+                //per.addEventListener('click',()=> setName(p.name));                
+                $(per).append(li);
+                $(listPersonagens).append(per);
+                cardHeader.append(dataHtml);
+            
+        }
+    });
+
+}
+
+const listar = () =>{
+    $('#pesquisar').val('');
+    getPersonagens();
+}
 
 const setName = (n) =>{
     localStorage.setItem('name', n);
